8197a732a742356f7d59ad5492deca19c3e8c05d
modestas612 <43955930+modestas612@users.noreply.github.com>
2019-04-12 19:31:17 +0300

    Updates

---
+++ precache-manifest.3067a96aec64941e849c73fdbe55dcdb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "b13fa10e90489a4fe527e2a8619a8c8f",
+    "url": "/Personal-portfolio/static/media/CV.b13fa10e.pdf"
+  },
+  {
+    "revision": "b236465d97cae9008295",
+    "url": "/Personal-portfolio/static/css/main.bee72367.chunk.css"
+  },
+  {
+    "revision": "53c61e5849fa3ec4df06",
+    "url": "/Personal-portfolio/static/js/runtime~main.7712ff0f.js"
+  },
+  {
+    "revision": "327db808d775f10f34a2041fa3d8d34e",
+    "url": "/Personal-portfolio/static/media/background1.327db808.jpg"
+  },
+  {
+    "revision": "8db0f08912baf8ef2de0",
+    "url": "/Personal-portfolio/static/js/2.aff2f8dc.chunk.js"
+  },
+  {
+    "revision": "0c90db4ba2ea4ee0ce7b4f5c66693261",
+    "url": "/Personal-portfolio/static/media/pic.0c90db4b.jpg"
+  },
+  {
+    "revision": "b236465d97cae9008295",
+    "url": "/Personal-portfolio/static/js/main.e6c15cbd.chunk.js"
+  },
+  {
+    "revision": "d31d9e15912f9e20afb2c2d942ff426b",
+    "url": "/Personal-portfolio/static/media/ComingSoonPage.d31d9e15.jpg"
+  },
+  {
+    "revision": "9fc3f92bfe8d367544cd9546ddb59c63",
+    "url": "/Personal-portfolio/static/media/MineSweeper.9fc3f92b.jpg"
+  },
+  {
+    "revision": "1937957967b780f954aacdad0c6ac4e1",
+    "url": "/Personal-portfolio/static/media/Portfolio.19379579.jpg"
+  },
+  {
+    "revision": "bc8ad7a16fa4ef17aec855e73b1b923a",
+    "url": "/Personal-portfolio/static/media/gallery.bc8ad7a1.jpg"
+  },
+  {
+    "revision": "8db0f08912baf8ef2de0",
+    "url": "/Personal-portfolio/static/css/2.cca63c7d.chunk.css"
+  },
+  {
+    "revision": "1a3a7fd5b079303bd358031d82257867",
+    "url": "/Personal-portfolio/index.html"
+  }
+];

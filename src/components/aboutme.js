343597556f97137dfbdd020eddd47b2d6d631ba3
modestas612 <43955930+modestas612@users.noreply.github.com>
2020-07-04 04:17:15 -0700

    Update aboutme.js
    changed about me desciption

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -18,9 +18,9 @@ export default class aboutme extends Component {
             <hr/>
           </div>
           <div className="content">
-            <p>As a developer with one years of experience writing code for applications and websites, I have a strong attention to detail as I know that even the simplest error can lead to lost production.</p>
-            <p>I am highly-motivated, self-driven, hard-working, fast learner. I eager to be a part of the team, learn new technologies, become a valuable employee of the company and bring it success.</p>
-            <p>Well-organized with an ability to prioritize tasks, develop and grow in the company. Experienced in communicating with the team and clients. Attentive and accurate to details.</p>
+            <p>If you are looking for a Java developer who will get things done in high quality, I would make an excellent candidate for it. I have a strong attention to detail as I know that even the simplest error can lead to lost production.</p>
+            <p>I am a passionate developer who can code well in Java programming language. For the last year I have been developing new functionality of business management system using Java and other technologies. There I improved my object-oriented principles and functional programming. I am highly-motivated, self-driven, hard-working, fast learner. I eager to be a part of the team, learn new technologies, become a valuable employee of the company and bring it success.</p>
+            <p>I always interested new programming trends and best practices. Well-organized with an ability to prioritize tasks, develop and grow in the company. Experienced in communicating with the team and clients. Attentive and accurate to details.</p>
           </div>
           <div className="footer">
             <div className="social-links">
